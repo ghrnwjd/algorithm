2603efe0fb7a92fe86b3e1f05db1e214a49625b4
HOYOUNG <hoyoung.wjd@gmail.com>
2024-01-26 15:00:43 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 45.43 ms, Memory: 134 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Solution {
+    public int[] solution(int [] arr) {
+
+        
+        Stack<Integer> stack = new Stack();
+        stack.add(arr[0]);
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] != stack.peek()) {
+                stack.add(arr[i]);
+            }
+        }
+        
+        return stack.stream().mapToInt(Integer::valueOf).toArray();
+    }
+}
